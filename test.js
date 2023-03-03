fd44c04df2f67e8b24f0e6d9b5141a7640eb74f8
Sushant Neupane <nsushant09@gmail.com>
2023-03-03 15:18:19 +0545

    commit

--- test.js
+++ test.js
@@ -6,8 +6,25 @@ btnClick.style.cursor = 'pointer';
 btnClick.addEventListener('click', () => {
     boolean = !boolean;
     if (boolean) {
-        showField.textContent = name;
+        showField.textContent = "Random String";
     } else {
         showField.textContent = "";
     }
 })
+
+btnClick.addEventListener('mouseover', ()=>{
+    btnClick.textContent = "Hovered"
+})
+btnClick.addEventListener('mouseout', ()=>{
+    btnClick.textContent = "Mouse out";
+})
+
+class Product{
+    constructor(product_id, name, stock, price, description){
+        this.product_id = product_id;
+        this.name = name;
+        this.stock = stock;
+        this.price;
+        this.description;
+    }
+}
